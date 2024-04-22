bef31ded0ea1ae20f36a56ab3534fbb919408bf5
22ftt1499 <22ftt1499@student.pb.edu.bn>
2024-04-22 23:42:16 +0800

    function login

---
+++ index.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="https://cdn.tailwindcss.com"></script>
+    <title>EP-QUEST</title>
+</head>
+
+<body class="bg-zinc-100 min-h-screen flex flex-col items-center justify-center p-4">
+    <div class="bg-zinc-100 p-4 rounded-lg w-full max-w-sm">
+        <div class="flex flex-col items-center">
+            <img src="assets/EpQuestLogo.png" alt="Logo" class="mb-2 size-44 logo">
+            <h1 class="text-xl font-bold mb-4">Ep-Quest</h1>
+        </div>
+        <form action="login.php" method="post" class="space-y-6">
+            <?php if (isset($_GET['error'])) { ?>
+                <p class="text-red-500 text-center"><?php echo $_GET['error']; ?></p>
+            <?php } ?>
+            <div class="relative">
+                <input type="text" name="stuid" placeholder="Student ID" class="pl-10 pr-4 py-2 w-full border rounded-full text-zinc-900 focus:outline-none focus:border-blue-500">
+                <span class="absolute left-3 top-2 text-zinc-500">
+                    <!-- <img src="https://placehold.co/20x20" alt="Logo" class="mb-2"> -->
+                </span>
+            </div>
+            <div class="relative">
+                <input type="password" name="password" placeholder="Password" class="pl-10 pr-4 py-2 w-full border rounded-full text-zinc-900 focus:outline-none focus:border-blue-500">
+                <span class="absolute left-3 top-2 text-zinc-500">
+                    <!-- <img src="https://placehold.co/20x20" alt="Logo" class="mb-2"> -->
+                </span>
+            </div>
+
+            <div class="flex justify-center">
+                <button type="submit" class="w-40 bg-blue-600 text-white py-2 rounded-full">Sign In</button>
+            </div>
+        </form>
+
+    </div>
+</body>
+
+</html>
--- login.php
+++ login.php
@@ -1,43 +1,48 @@
 <?php
+session_start();
 
+include 'db_conn.php';
+if (isset($_POST['stuid']) && isset($_POST['password'])) {
 
-?>
+    function validate($data)
+    {
+        $data = trim($data);
+        $data = stripslashes($data);
+        $data = htmlspecialchars($data);
+        return $data;
+    }
 
-<!DOCTYPE html>
-<html lang="en">
+    $stuid = validate($_POST['stuid']);
+    $password = validate($_POST['password']);
 
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <script src="https://cdn.tailwindcss.com"></script>
-    <title>EP-QUEST</title>
-</head>
+    if (empty($stuid)) {
+        header('Location: index.php?error=Student ID is required');
+        exit();
+    } else if (empty($password)) {
+        header('Location: index.php?error=Password is required');
+        exit();
+    } else {
+        $sql = "SELECT * FROM students WHERE student_id='$stuid' AND password='$password'";
+        $result = mysqli_query($conn, $sql);
 
-<body class="bg-zinc-100 min-h-screen flex flex-col items-center justify-center p-4">
-    <div class="bg-zinc-100 p-4 rounded-lg w-full max-w-sm">
-        <div class="flex flex-col items-center">
-            <img src="assets/EpQuestLogo.png" alt="Logo" class="mb-2 size-44">
-            <h1 class="text-xl font-bold mb-4">Ep-Quest</h1>
-        </div>
-        <form class="space-y-6">
-            <div class="relative">
-                <input type="text" placeholder="Student ID" class="pl-10 pr-4 py-2 w-full border rounded-full text-zinc-900 focus:outline-none focus:border-blue-500">
-                <span class="absolute left-3 top-2 text-zinc-500">
-                    <!-- <img src="https://placehold.co/20x20" alt="Logo" class="mb-2"> -->
-                </span>
-            </div>
-            <div class="relative">
-                <input type="password" placeholder="Password" class="pl-10 pr-4 py-2 w-full border rounded-full text-zinc-900 focus:outline-none focus:border-blue-500">
-                <span class="absolute left-3 top-2 text-zinc-500">
-                    <!-- <img src="https://placehold.co/20x20" alt="Logo" class="mb-2"> -->
-                </span>
-            </div>
-            <div class="flex justify-center">
-                <button type="submit" class="w-40 bg-blue-600 text-white py-2 rounded-full">Sign In</button>
-            </div>
-        </form>
-    </div>
-</body>
-
-
-</html>
+        if (mysqli_num_rows($result) === 1) {
+            $row = mysqli_fetch_assoc($result);
+            if ($row['student_id'] === $stuid && $row['password'] === $password) {
+                $_SESSION['student_id'] = $row['student_id'];
+                $_SESSION['name'] = $row['name'];
+                $_SESSION['id'] = $row['id'];
+                header('Location: studentpage.php');
+                exit();
+            } else {
+                header('Location: index.php?error=Incorrect Student ID or Password');
+                exit();
+            }
+        } else {
+            header('Location: index.php?error=Incorrect Student ID or Password');
+            exit();
+        }
+    }
+} else {
+    header('Location: index.php');
+    exit();
+}
