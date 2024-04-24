8df0d10b804d1e60dc6f0ab2eeba6a000890a08b
22FTT1500 <22FTT1500@student.pb.edu.bn>
2024-04-25 01:03:06 +0800

    create event page

---
+++ eventpage.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="https://cdn.tailwindcss.com"></script>
+    <title>Document</title>
+</head>
+
+<body>
+    <img class="w-full h-56" src="https://placehold.co/500x300/1D4ED8/FFFFFF?text=Esport+Event" alt="Esport Event">
+
+    <div class="p-6 text-black">
+        <h2 class="text-2xl font-bold text-black dark:text-white">Esport Event</h2>
+        <p class="mt-2 text-sm text-black dark:text-white">Assalamualaikum everyone and a very Good Morning,</p>
+        <p class="text-sm text-black dark:text-white">We would like to encourage students to join PESTA CONVO ESPORTS 🎮 on the following dates given attached on the poster.</p>
+
+        <div class="mt-4">
+            <div class="text-teal-600 dark:text-black-400">
+                <p>13th - 15th OCTOBER</p>
+                <ul class="list-disc pl-5">
+                    <li>MLBB TOURNAMENT: 1ST DAY AND 2ND DAY (ONLINE)</li>
+                    <li>3RD DAY (OFFLINE)</li>
+                    <li>VENUE FOR MLBB OFFLINE: TBC</li>
+                    <li>TEKKEN TOURNAMENT: POLITEKNIK, ONG SUM PING</li>
+                </ul>
+            </div>
+            <div class="mt-4 text-teal-600 dark:text-black-400">
+                <p>16th - 18th OCTOBER</p>
+                <ul class="list-disc pl-5">
+                    <li>PUBGM AND VALORANT TOURNAMENT</li>
+                    <li>VENUE WILL BE HELD: ONLINE</li>
+                </ul>
+            </div>
+        </div>
+
+        <button class="w-full px-4 py-2 mt-8 text-sm font-medium tracking-wide text-white capitalize transition-colors duration-200 transform bg-blue-600 rounded-md hover:bg-blue-500 focus:outline-none focus:bg-blue-500">
+            Join Now
+        </button>
+    </div>
+</body>
+
+</html>
