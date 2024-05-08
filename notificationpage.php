329a4a8430065aa436a5b4abf9cae731d3f3a60d
22FTT1500 <22FTT1500@student.pb.edu.bn>
2024-05-08 15:11:38 +0800

    sa

---
+++ notificationpage.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+
+</head>
+
+<body class="bg-zinc-100 dark:bg-zinc-800">
+    <div class="max-w-sm mx-auto">
+
+        <div class="flex items-center justify-between p-4 bg-white dark:bg-zinc-900 shadow">
+            <button class="text-blue-500">
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                    <path stroke-linecap="round" stroke-linejoin="round" d="M19.5 12h-15m0 0l6-6m-6 6l6 6" />
+                </svg>
+            </button>
+            <h1 class="text-lg font-semibold text-zinc-900 dark:text-white">Notification</h1>
+            <button class="text-blue-500">
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                    <path stroke-linecap="round" stroke-linejoin="round" d="M12 6v12m6-6H6" />
+                </svg>
+            </button>
+        </div>
+
+
+        <div class="mt-4 space-y-2">
+            <div class="bg-white dark:bg-zinc-900 p-4 shadow rounded-lg">
+                <h2 class="text-lg font-bold text-zinc-900 dark:text-white">Join Movie Club Now 🍿</h2>
+                <p class="text-zinc-500 dark:text-zinc-400">14/02/2024 10:30 AM</p>
+            </div>
+            <div class="bg-white dark:bg-zinc-900 p-4 shadow rounded-lg">
+                <h2 class="text-lg font-bold text-zinc-900 dark:text-white">PUBGM AND VALORANT TOURNAMENT</h2>
+                <p class="text-zinc-500 dark:text-zinc-400">14/02/2024 10:30 AM</p>
+            </div>
+            <div class="bg-white dark:bg-zinc-900 p-4 shadow rounded-lg">
+                <h2 class="text-lg font-bold text-zinc-900 dark:text-white">HANDBALL CLUB OFFERS 3K HOLY !!!</h2>
+                <p class="text-zinc-500 dark:text-zinc-400">14/02/2024 10:30 AM</p>
+            </div>
+        </div>
+    </div>
+</body>
+
+</html>
