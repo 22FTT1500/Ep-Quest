561293bf1cdb16951b0f0f4eda10bdbcede3eb88
Nabilah Jalil <22FTT1506@student.pb.edu.bn>
2024-05-10 12:29:37 +0800

    aboutpage css done

--- aboutpage.php
+++ aboutpage.php
@@ -8,52 +8,56 @@
     <title>Document</title>
 </head>
 
-<body class="bg-zinc-900 text-white font-sans">
+<body class="bg-white-100/50 bg-cover bg-repeat bg-center text-white font-sans" style="background-image: url('./assets/background.png');">
 
     <div class="flex flex-col h-screen">
-        <div class="p-4 bg-blue-900">
+        <div class="py-6 px-4 bg-sky-400 rounded-br-[15px] rounded-bl-[15px] border-2 border-black">
             <!-- Profile Link -->
             <div class="flex items-center justify-between">
                 <div class="flex items-center">
                     <a href="settingpage.php"><img src="./assets/left-arrow.png" alt="profile" class="rounded-full ml-3 size-7" /></a>
                     <!-- add center title -->
                 </div>
-                <div class="justify-self-center ml-3">
-                    <h1 class="font-bold">Manage Profile</h1>
+                <div class="justify-self-center ml-3 text-white text-lg">
+                    <h1 class="font-bold">About Us</h1>
                 </div>
                 <div>
                     <img src="./assets/bell.png" alt="profile" class="rounded-full ml-3 size-7" />
                 </div>
             </div>
         </div>
-        <div class="flex flex-col items-center">
-            <div class="bg-white rounded-full p-2 border-2 border-blue-500 mb-4">
-                <img src="assets/EpQuestLogo.png" alt="EP Quest Logo">
+        <div class="flex flex-col items-center p-4">
+            <div>
+                <img src="assets/EpQuestLogo.png" alt="EP Quest Logo" class="size-48">
             </div>
-            <h2 class="text-2xl font-bold text-blue-500">Ep-Quest</h2>
+            <h2 class="text-2xl font-serif font-bold text-white bg-sky-400 rounded-full py-2 px-8">Ep-Quest</h2>
         </div>
-        <div class="mt-4">
-            <h3 class="text-lg font-semibold">About Us</h3>
-            <p class="text-sm">
-                EP Quest is a student app for tracking enrichment points, allowing students to earn points for educational tasks and activities. This solution helps schools reduce inventory losses by allowing students to check their current points without waiting for the next semester.
-            </p>
+        <div class="py-2 px-6">
+            <div class="bg-white rounded-[20px] rounded-bl-[50px] border-2 border-black pb-8 py-2 px-2 flex flex-col text-justify">
+                <div class="mt-4 text-black">
+                    <h3 class="text-xl font-semibold pb-2">About Us</h3>
+                    <p class="text-sm text-slate-500">
+                        EP Quest is a student app for tracking enrichment points, allowing students to earn points for educational tasks and activities. This solution helps schools reduce inventory losses by allowing students to check their current points without waiting for the next semester.
+                    </p>
+                </div>
+                <div class="mt-4 text-black">
+                    <h3 class="text-xl font-semibold pb-2">Mission</h3>
+                    <p class="text-sm text-slate-500">
+                        EP Quest aims to motivate college students to take charge of their educational lives and become active, stimulated competitors.
+                    </p>
+                </div>
+                <div class="mt-4 mb-4 text-black">
+                    <h3 class="text-xl font-semibold pb-2">Vision</h3>
+                    <p class="text-sm text-slate-500">
+                        EP Quest is a student point tracking app created for improving student engagement and motivation in educational settings.
+                    </p>
+                </div>
+            </div>
         </div>
-        <div class="mt-4">
-            <h3 class="text-lg font-semibold">Mission</h3>
-            <p class="text-sm">
-                EP Quest aims to motivate college students to take charge of their educational lives and become active, stimulated competitors.
-            </p>
-        </div>
-        <div class="mt-4 mb-4">
-            <h3 class="text-lg font-semibold">Vision</h3>
-            <p class="text-sm">
-                EP Quest is a student point tracking app created for improving student engagement and motivation in educational settings.
-            </p>
-        </div>
-        <div class="flex justify-center items-center">
+        <!-- <div class="flex justify-center items-center">
             <button class="text-blue-500">&#x2715;</button>
-        </div>
-        <div class="fixed bottom-0 left-0 right-0 bg-zinc-900 p-4 text-center">
+        </div> -->
+        <div class=" bg-sky-400 rounded-tr-[15px] rounded-tl-[15px] border-2 border-black font-bold fixed bottom-0 left-0 right-0 p-4 text-center">
             <p>Contact Us: +673 2020900</p>
         </div>
     </div>
