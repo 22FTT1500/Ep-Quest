784ba1dd5d25329251e034c7c96079790967f1f1
Nabilah Jalil <22FTT1506@student.pb.edu.bn>
2024-05-10 13:58:12 +0800

    eprecord css done

--- eprecord.php
+++ eprecord.php
@@ -8,40 +8,50 @@
 </head>
 
 <body>
-    <div class="bg-zinc-900 text-black font-sans"> <!-- Changed text color to black -->
-        <div class="flex flex-col h-screen">
+    <div class="bg-cover bg-repeat bg-center text-white font-sans" style="background-image: url('./assets/background.png');">
+        <div class="flex flex-col h-screen text-black">
 
-            <div class="flex justify-between items-center bg-blue-500 text-white p-4 rounded-t-lg">
-                <button class="text-2xl">←</button>
-                <h1 class="text-xl font-bold">Ep Records</h1>
-                <button>🔔</button>
+            <div class="flex justify-between items-center py-8 px-4 bg-sky-400 rounded-br-[30px] rounded-bl-[30px] border-2 border-black">
+
+                <a href="activitypage.php"><img src="./assets/left-arrow.png" alt="back" class="rounded-full ml-3 size-7" /></a>
+
+                <h1 class="text-xl font-bold text-white">Ep Records</h1>
+
+                <img src="./assets/bell.png" alt="bell" class="rounded-full ml-3 size-7" />
+
             </div>
 
-
-            <div class="bg-white p-4 rounded-b-lg shadow-lg mb-4">
-                <div class="flex justify-center mb-4">
-                    <img src="https://placehold.co/300x150" alt="Handball Image">
+            <div class="flex flex-col gap-4 py-6 px-4">
+                <div class="bg-white p-6 rounded-[30px] shadow-lg mb-4 border-2 border-black">
+                    <div class="flex justify-center mb-4" style="border-bottom: 2px solid black;">
+                        <img src="https://placehold.co/300x150" alt="Handball Image" style="object-fit: cover; width: 100%; height: 100%;">
+                    </div>
+                    <div class="flex flex-col gap-2 text-lg">
+                        <p class="font-bold flex justify-between" style="border-bottom: 1px solid black;">Club: <span class="font-normal text-right">Handball</span></p>
+                        <p class="font-bold flex justify-between" style="border-bottom: 1px solid black;">Attendance: <span class="font-normal text-right">Attend ✔</span></p>
+                        <p class="font-bold flex justify-between" style="border-bottom: 1px solid black;">Sign In: <span class="font-normal text-right">9:50 AM</span></p>
+                        <p class="font-bold flex justify-between" style="border-bottom: 1px solid black;">Date: <span class="font-normal text-right">26 Oct 2023</span></p>
+                        <p class="font-bold flex justify-between">Ep Gained: <span class="font-normal text-right">+4</span></p>
+                    </div>
                 </div>
-                <p class="font-bold">Club: <span class="font-normal">Handball</span></p>
-                <p class="font-bold">Attendance: <span class="font-normal">Attend ✔</span></p>
-                <p class="font-bold">Sign In: <span class="font-normal">9:50 AM</span></p>
-                <p class="font-bold">Date: <span class="font-normal">26 Oct 2023</span></p>
-                <p class="font-bold">Ep Gained: <span class="font-normal">+4</span></p>
-            </div>
 
-
-            <div class="bg-white p-4 rounded-lg shadow-lg">
-                <div class="flex justify-center mb-4">
-                    <img src="https://placehold.co/300x150" alt="Esport Event Image">
+                <div class="bg-white p-4 rounded-[30px] shadow-lg mb-4 border-2 border-black">
+                    <div class="flex justify-center mb-4" style="border-bottom: 2px solid black;">
+                        <img src="https://placehold.co/300x150" alt="Esport Event Image" style="object-fit: cover; width: 100%; height: 100%;">
+                    </div>
+                    <div class="flex flex-col gap-2 text-lg">
+                        <p class="font-bold flex justify-between" style="border-bottom: 1px solid black;">Event: <span class="font-normal text-right">Esport Event</span></p>
+                        <p class="font-bold flex justify-between" style="border-bottom: 1px solid black;">Attendance: <span class="font-normal text-right">Attend ✔</span></p>
+                        <p class="font-bold flex justify-between" style="border-bottom: 1px solid black;">Sign In: <span class="font-normal text-right">9:50 AM</span></p>
+                        <p class="font-bold flex justify-between" style="border-bottom: 1px solid black;">Date: <span class="font-normal text-right">26 Oct 2023</span></p>
+                        <p class="font-bold flex justify-between">Ep Gained: <span class="font-normal text-right">+5</span></p>
+                    </div>
                 </div>
-                <p class="font-bold">Event: <span class="font-normal">Esport Event</span></p>
-                <p class="font-bold">Attendance: <span class="font-normal">Attend ✔</span></p>
-                <p class="font-bold">Sign In: <span class="font-normal">9:50 AM</span></p>
-                <p class="font-bold">Date: <span class="font-normal">26 Oct 2023</span></p>
-                <p class="font-bold">Ep Gained: <span class="font-normal">+5</span></p>
             </div>
+
+
         </div>
-
+        <div class=" bg-sky-400 rounded-tr-[15px] rounded-tl-[15px] border-2 border-black font-bold fixed bottom-0 left-0 right-0 p-4 text-center"> </div>
 </body>
 
 </html>
