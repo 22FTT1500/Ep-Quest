a8e53dd27f54d4787f24898144c1e50f28da7715
22ftt1499 <22ftt1499@student.pb.edu.bn>
2024-05-11 01:13:29 +0800

    update login page

--- login.php
+++ login.php
@@ -2,6 +2,7 @@
 session_start();
 
 include 'db_conn.php';
+
 if (isset($_POST['stuid']) && isset($_POST['password'])) {
 
     function validate($data)
@@ -27,11 +28,9 @@ if (isset($_POST['stuid']) && isset($_POST['password'])) {
 
         if (mysqli_num_rows($result) === 1) {
             $row = mysqli_fetch_assoc($result);
-            if ($row['student_id'] === $stuid && $row['password'] === $password) {
+            if ($row['is_admin'] == 0) { // Check if user is admin
                 $_SESSION['student_id'] = $row['student_id'];
                 $_SESSION['fullname'] = $row['fullname'];
-                $_SESSION['name'] = $row['name'];
-                $_SESSION['id'] = $row['id'];
                 $_SESSION['grpcode'] = $row['grpcode'];
                 $_SESSION['email'] = $row['email'];
                 $_SESSION['contactno'] = $row['contactno'];
@@ -39,11 +38,26 @@ if (isset($_POST['stuid']) && isset($_POST['password'])) {
                 $_SESSION['profileimg'] = $row['profileimg'];
                 $_SESSION['point'] = $row['point'];
                 $_SESSION['total_ep_points'] = $row['total_ep_points'];
+                $_SESSION['is_admin'] = true; // Set admin session variable
+
+                header('Location: adminpage.php');
+                exit();
+            } else if ($row['is_student'] == 1) { // Check if user is student
+                $_SESSION['student_id'] = $row['student_id'];
+                $_SESSION['fullname'] = $row['fullname'];
+                $_SESSION['grpcode'] = $row['grpcode'];
+                $_SESSION['email'] = $row['email'];
+                $_SESSION['contactno'] = $row['contactno'];
+                $_SESSION['course'] = $row['course'];
+                $_SESSION['profileimg'] = $row['profileimg'];
+                $_SESSION['point'] = $row['point'];
+                $_SESSION['total_ep_points'] = $row['total_ep_points'];
+                $_SESSION['is_admin'] = false; // Set admin session variable to false for student
 
                 header('Location: studentpage.php');
                 exit();
             } else {
-                header('Location: index.php?error=Incorrect Student ID or Password');
+                header('Location: index.php?error=Account not authorized');
                 exit();
             }
         } else {
